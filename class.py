8d5180fdba74076ea0180eccecd3422547b897da
fares sheikhy <faressheikhy@gmail.com>
2026-08-16 00:22:20 +0330

    initail commit

---
+++ class.py
@@ -0,0 +1,40 @@
+name = "Class"
+student_count = 30
+class_active = True
+NUMPI = 3.14
+print(name)
+print(student_count)
+print(class_active)
+# inputs for class students
+# input for one of the students
+name = "Shahin"
+age = 18
+print(name)
+print(age)
+name = input("Enter your name:Shahin")
+age = input("Enter your age:18")
+# mathematics of the class students
+student_count = 30
+total_students = student_count * 1  # Assuming each student contributes 1 to the total
+print(total_students)
+a = 10  # assuming a is 10 of the students
+b = 20  # assuming b is 20 of the students
+# Perform mathematical operations to mutiply,divide,add,subtract,modulus,exponentiation,floor division
+print(a + b)
+print(a * b)
+print(a / b)
+print(a - b)
+print(a ** b)
+print(a % b)
+# Perform logical operations to check to equality,greater than,less than,greater than or equal to,less than or equal to
+print(a == b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+# Perform logical operations to check to equality,greater than,less than,greater than or equal to,less than or equal to for Shahin's age
+age = 18
+print(age < 25 and age > 18)
+print(age < 25 or age > 18)
+print(not(age < 25))
+print(not(age > 18))
